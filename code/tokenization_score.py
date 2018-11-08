9fe34897543a2ac22e13b483be4bbec0962e0098
Ehsan <sehsanm@gmail.com>
2018-11-08 12:30:25 +0330

    Morph scoring

--- code/tokenization_score.py
+++ code/tokenization_score.py
@@ -1,6 +1,47 @@
 import csv
 import os
 
+mapping = {
+    'A0': 'A',
+    'A1': 'A',
+    'A2': 'A',
+    'Ab': 'A',
+    'Ad': 'A',
+    'Al': 'A',
+    'C0': 'C',
+    'C1': 'C',
+    'EXP': 'Exp',
+    'INTJ': 'Intj',
+    'N1': 'N',
+    'N2': 'N',
+    'N3': 'N',
+    'N4': 'N',
+    'N5': 'N',
+    'N6': 'N',
+    'N7': 'N',
+    'N8': 'N',
+    'N9': 'N',
+    'NA': 'N',
+    'NO': 'No',
+    'NU': 'No',
+    'PO': 'P',
+    'PR': 'P',
+    'PR1': 'P',
+    'SI': 'S',
+    'SP': 'S',
+    'V1': 'V',
+    'V2': 'V',
+    'V3': 'V',
+    'V4': 'V',
+    'V5': 'V',
+    'V6': 'V',
+    'VPR': 'VPr',
+    'PL': 'Pl',
+    'Y': 'Y',
+    'NEG': 'Neg',
+    'O': 'O'
+}
+
 
 def read_conll_file(address):
     ret = []
@@ -31,7 +72,7 @@ def read_conll_file(address):
                 line = f.readline()
                 line_number += 1
         except:
-            print(f'Error Parsing the file: {address} Error {args}')
+            print(f'Error Parsing the file: {address}')
 
     return ret
 
@@ -60,22 +101,38 @@ def normal_string_matcher(s1 , s2):
         return 0
 
 
-def morph_matcher(s1 , s2):
-    if s1[0] == '_' or s2[0] == '_' or s1[1] == '_' or s2[1] == '_':
+def set_matcher(s1 , s2):
+    if s2 == '_' or s1 == '_':  # As _ is equal to empty we will not match them!
         return 0
+    return len(set(s1.replace('#' , '|').replace(' ' , '').split('|'))\
+        .intersection(set(s2.replace('#' , '|').replace(' ' , '').split('|'))))
+def morph_matcher(s1 , s2):
+
+    if len(s1) < 2 or len(s2) < 2 or  s1[0] == '_' or s2[0] == '_' or s1[1] == '_' or s2[1] == '_':
+        return 0
+    return len(build_morph_set(s1[0] , s1[1]).intersection(build_morph_set(s2[0], s2[1])))
 
 
 def build_morph_set(struct, analyis):
     struct_parts = struct.replace(' ', '').split('|')
     analyis_parts = analyis.replace(' ', '').lower().split('|')
     ret = set()
-    for i in range(min(len(struct_parts, analyis_parts))):
+
+    for i in range(min(len(struct_parts), len(analyis_parts))):
         ret.add(struct_parts[i] + analyis_parts[i])
         ret.add(struct_parts[i] + reduced_tag(analyis_parts[i]))
+    return ret
 
 
 def reduced_tag(s):
-    
+    ret = []
+    parts = s.upper().replace(' ', '').split('+')
+    for p in parts:
+        if p in mapping:
+            ret.append(mapping[p])
+        else:
+            ret.append('O')
+    return '+'.join(ret)
 
 def longest_common_subsequence_general(s1, s2, matcher):
     m = len(s1)
@@ -145,7 +202,8 @@ def extract_scores():
         writer = csv.writer(csvfile, delimiter=',',
                             quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(
-            ['Team', 'Reference Team', 'Total Tokens', 'Segment Score', 'Token Score', 'Stem Score', 'Lemma Score'])
+            ['Team', 'Reference Team', 'Total Tokens', 'Segment Score', 'Token Score', 'Stem Score', 'Lemma Score',
+             'Morph Score' , 'Morph Analysis Score'])
         for r in all_results:
             writer.writerow(r)
 
@@ -153,8 +211,11 @@ def extract_scores():
 def reduce_column(lst, column):
     ret = []
     if isinstance(column, list):
+        ret = [[] for x in range(len(lst))]
         for c in column:
-            ret.append(reduce_column(lst, c))
+            xx = reduce_column(lst, c)
+            for i , v in enumerate(xx):
+                ret[i].append(v)
     else:
         for l in lst:
             if len(l) == 0:
@@ -173,18 +234,22 @@ def score_file(output_file, ref_file):
     token_score = compare_term(ref_items, out_items, 1)
     stem_score = compare_term(ref_items, out_items, 2)
     lemma_score = compare_term(ref_items, out_items, 3)
-
+    morph_score = longest_common_subsequence_general(reduce_column(ref_items , 4),
+                                                     reduce_column(out_items , 4),
+                                                     set_matcher)
+    morph_analysis_score = longest_common_subsequence_general(reduce_column(ref_items , [4, 5]),
+                                                     reduce_column(out_items , [4, 5]),
+                                                     morph_matcher)
     print('Segment Score', segment_score, 'Token score:', token_score, ' Stem Score: ', stem_score, ' Lemma Score: ',
-          lemma_score)
-    return [len(ref_items), segment_score, token_score, stem_score, lemma_score]
+          lemma_score, ' Morph Score: ' ,  morph_score, ' Analysis Score: ' , morph_analysis_score)
+    return [len(ref_items), segment_score, token_score, stem_score, lemma_score , morph_score, morph_analysis_score]
 
 
 def main():
     print('Scoring tool for SBU NLP Project')
-    # score_file('../data/sample/sample.out', '../data/sample/sample.ref')
+    #score_file('../data/sample/sample.out', '../data/sample/sample.ref')
     extract_scores()
 
 
 if __name__ == "__main__":
     main()
-    
